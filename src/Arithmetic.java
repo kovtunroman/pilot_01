5f6b674fe0bf1cf0da55a1ec0a4ac24f841e0cdf
kovtunroman <9696465@gmail.com>
2022-12-16 21:11:20 +0300

    Ariphmetic class added

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,2 +1,33 @@
 public class Arithmetic {
+    public static double sumOperation (double ... args){
+        double sum = 0;
+        for (int i = 0; i < args.length; i++){
+            sum += args[i];
+        }
+        return sum;
+    }
+
+    public static double substractOperation (double ... args){
+        double sum = args[0];
+        for (int i = 1; i < args.length; i++){
+            sum -= args[i];
+        }
+        return sum;
+    }
+
+    public static double multiplyOperation (double ... args){
+        double sum = args[0];
+        for (int i = 1; i < args.length; i++){
+            sum *= args[i];
+        }
+        return sum;
+    }
+
+    public static double devideOperation (double ... args){
+        double sum = args[0];
+        for (int i = 1; i < args.length; i++){
+            sum /= args[i];
+        }
+        return sum;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,17 @@ public class Main {
         jeffCat.displayInfo();
 
         Vehicle plane = new Vehicle("Plane", "Turbine", 1455, true);
+
+        System.out.println();
+        System.out.printf(Double.toString(Arithmetic.sumOperation(1,3,4)));
+
+        System.out.println();
+        System.out.printf(Double.toString(Arithmetic.substractOperation(1,2)));
+
+        System.out.println();
+        System.out.printf(Double.toString(Arithmetic.multiplyOperation(2,2)));
+
+        System.out.println();
+        System.out.printf(Double.toString(Arithmetic.devideOperation(4,2)));
     }
 }
